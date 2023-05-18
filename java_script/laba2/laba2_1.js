f1a123d755f3f0c5778b24f5dc12ff3be850dd07
SokolovaPolina <polina.sokolova.02bk.ru@gmail.com>
2023-05-18 09:49:34 +0300

    add laba2_1

--- java_script/laba2/laba2_1.js
+++ java_script/laba2/laba2_1.js
@@ -1,3 +1,22 @@
+/**
+ * Возвращает x, возведённое в n-ную степень.
+ *
+ * @param {number} x Возводимое в степень число.
+ * @param {number} n Степень, должна быть натуральным числом.
+ * @return {number} x, возведённое в n-ную степень.
+ */
 function pow(x, n) {
     return x**n;
+  }
+/**
+ * Возвращает сумму чисел от 1 до n включительно.
+ *
+ * @param {number} n Вводимое число.
+ * @return {number} sum, сумма от 1 до n.
+ */
+  function sumTo(n) {
+    while(i <= n) {
+        sum += 1; 
+    }
+    return sum;
   }
