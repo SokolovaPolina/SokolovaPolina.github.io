07e32d6403ea38aeb89e1c178e5cc4326d450d29
SokolovaPolina <polina.sokolova.02bk.ru@gmail.com>
2023-05-18 10:35:42 +0300

    add laba2

--- java_script/laba2/laba2_1.js
+++ java_script/laba2/laba2_1.js
@@ -14,9 +14,20 @@ function pow(x, n) {
  * @param {number} n Вводимое число.
  * @return {number} sum, сумма от 1 до n.
  */
-  function sumTo(n) {
+    function sumTo(n) {
+    let i = 0;
+    let sum = 0;
     while(i <= n) {
-        sum += 1; 
+        sum = n * (n + 1) / 2;
+        i++;
     }
     return sum;
+  }
+  function factorial(n) {
+    if (n <= 1) {
+        return 1;
+    } else {
+        res = n * factorial(n - 1);
+    }
+    
   }
