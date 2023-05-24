95775998bbc53b36b8ae64d4412de1e75d45a98c
SokolovaPolina <polina.sokolova.02bk.ru@gmail.com>
2023-05-24 21:34:59 +0300

    add laba3

--- java_script/laba3/lab3.js
+++ java_script/laba3/lab3.js
@@ -5,7 +5,7 @@
 * @return {number} sum, возвращает дробную часть числа.
 */
 function getDecimal(num){
-   let res =0;
+   let res = 0;
    if (num >= 0) {
       let cel = Math.trunc(num);
       res = num - cel;
@@ -56,15 +56,64 @@ function truncate(str, maxlength){
 * преобразуйте строку вида 'var-test-text' в 'varTestText'
 *
 * @param {string} str Вводимая строка.
-* @param {number} maxlength вводимая максимальная длина строки.
-* @param {string} strNew новая усеченная строка.
-* @return {string} strNew str, результатом функции должна быть (при необходимости) усечённая строка
+* @param {Array} strArr полученный список из строки, сформированный по разделителю "-".
+* @return {string} strNew, новая строка
 */
 function camelize(str){
-   let target = "-";
-   let pos = -1;
-   while((pos = str.indexOf(target, pos + 1)) != -1){
-      strNew = str ;
-   }
-
+   //let str = "fgcf-gjdt-cg";
+   let strArr = str.split("-");
+     for (let i = 0; i <= strArr.length - 1; i++){
+      if (i == 0) {
+         strNew = strArr[0];
+      } else strNew += ucFirst(strArr[i]);
+      
+      } return strNew;   
+      
+}  
+/**
+* для натурального n возвращает массив, заполненный числами Фибоначчи до n-го (не включая его)
+*
+* @param {number} n, целое натуральное число.
+* @param {array} arr, пустой массив
+* @return {array} arr, массив, заполненный числами Фибоначчи до n-го (не включая его)
+*/
+function fibs(n) {
+   let arr = [];
+   for (let i = 0; i < n; i++){
+      arr.push(fib(i));
+   } return (arr);  
 }
+/**
+* принимает неупорядоченный массив чисел arr и возвращает массив, упорядоченный по убыванию
+*
+* @param {array} arr, неупорядоченный массив чисел
+* @return {array} arr, массив, упорядоченный по убыванию
+*/
+
+function arrReverseSorted(arr) {
+   /**
+*  упорядочение по убыванию
+*
+* @param {number} a, b, пары чисел, проверяемые между собой в массиве
+* @return {number} -1, 0, 1
+*/
+   function compareNumeric(a, b) {
+      if (a > b) return -1;
+      if (a == b) return 0;
+      if (a < b) return 1;
+    }
+    arr.sort(compareNumeric);
+    return arr;
+
+   }
+/**
+*  возвращает массив уникальных, не повторяющихся значений массива arr
+*
+* @param {any} arr,зЗначения в массиве могут быть любого типа
+* @param {array} arr, пустой массив
+* @return {array} arr, массив уникальных значений
+*/
+function unique(arr) {
+   return Array.from(new Set(arr));
+}
+
